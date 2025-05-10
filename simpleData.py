234c96ae15595be995770bc169afbc4d3bbe6d79
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-05-10 23:59:25 +1000

    check the length of two strings

---
+++ simpleData.py
@@ -0,0 +1,19 @@
+# find Length:
+Line1 = "ONLNE DIRECTORY/SOFTWARE DVLPMNT, PRJCT"
+Line2 = "MANAGEMENT & CONSLTING SVSC(HBB/ON CALL)"
+print(len(Line1))
+print(len(Line2))
+Line1 = "Online directory, advertising, promotions"
+Line2 = "& related services"
+print(len(Line1))
+print(len(Line2))
+print((Line1.upper()))
+print((Line2.upper()))
+Line1 = "ONLINE DIRECTORY,ADVERTISING,PROMOTIONS"
+Line2 = "& RELATED SERVICES (HBB/ON CALL)"
+print(len(Line1))
+print(len(Line2))
+Line1 = "ONLINE DIRECTORY,ADVERTISING,PROMOTIONS"
+Line2 = "& RELATED SERVICES (HBB/ON CALL)"
+print(len(Line1))
+print(len(Line2))
